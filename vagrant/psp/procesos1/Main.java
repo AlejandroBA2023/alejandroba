a06fbff9e695f3f2ef22d0b278c3a660751f95ed
AlejandroBA2023 <alejandroba@iescastelar.com>
2023-10-02 14:13:25 +0200

    Ejercicio 1 comentado

--- vagrant/psp/procesos1/Main.java
+++ vagrant/psp/procesos1/Main.java
@@ -3,21 +3,27 @@ import java.util.Random;
 public class Main{
         public static void main(String[] args){
                 try {
-                        //ProcessBuilder pb = new ProcessBuilder("/vagrant/procesos1/esperarNsegundos.sh", "4");
-                        //Process p = pb.start();
+                        // DECLARACION Y EJECUCION DEL PROCESO
                         Process p = Runtime.getRuntime().exec("/bin/bash esperarNsegundos.sh " + args[0]);
+
+                        // LLAMA A LA FUNCION LETRA ALEATORIA MIENTRAS EL PROCESO ESTA VIVO
                         while (p.isAlive()) {
                             letraAleatoria();
                         }
+
+                        // FINALIZACION DEL PROCESO
                         p.waitFor();
                         p.destroy();
+
                 } catch (Exception e) {
                         e.printStackTrace();
                 }
 
         }
 
-                
+
+
+         //* FUNCION RECUPERADA Y MODIFICADA DE "https://www.baeldung.com/java-random-string"
         private static void letraAleatoria() {
             int leftLimit = 97; // letter 'a'
             int rightLimit = 122; // letter 'z'
